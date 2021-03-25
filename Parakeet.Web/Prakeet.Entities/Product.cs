6f7943723f13e12864263f3d16fe218b623daf0c
Samir <34768724+AcrobaticSam@users.noreply.github.com>
2021-03-25 21:30:58 +0600

    Created Entities
    Created base entities, simply put properties that we must need to take input from user and then show it later on web.

---
+++ Parakeet.Web/Prakeet.Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakeet.Entities
+{
+    public class Product : BaseEntity
+    {
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        [MaxLength(50000)]
+        public string Ingredients { get; set; }
+
+        [MaxLength(50000)]
+        public string Analysis { get; set; }
+
+
+        [MaxLength(50000)]
+        public string Info { get; set; }
+
+
+        [MaxLength(50000)]
+        public string Intro { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string ImageURL2 { get; set; }
+
+        public string ImageURL3 { get; set; }
+
+        public string ImageURL4 { get; set; }
+    }
+}
---
+++ Parakeet.Web/Prakeet.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prakeet.Entities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prakeet.Entities")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b0a71e1-264c-4f3a-85af-73012ee54466")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
